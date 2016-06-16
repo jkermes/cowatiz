e8cae3909a01a1ccbe3ae7659caa5f9fb6939e11
Julien Kermes <julien.kermes@gmail.com>
2016-06-16 17:23:34 +0200

    Added an image upload service.

--- www/js/services/storage.js
+++ www/js/services/storage.js
@@ -6,15 +6,42 @@ angular.module('starter.services.storage', ['firebase'])
 	var deferred = $q.defer();
 	var promise = deferred.promise;
 
+	var imagesRef = storageRef.child('images/users');
+
 	return {
-		getUserImage: function() {
-			var imagesRef = storageRef.child('images/users');
-			var userImageRef = imagesRef.child('adam.jpg');
+		getUserImage: function(uid) {
+
+			var userImageRef = imagesRef.child(uid + ".jpg");
 
 			userImageRef.getDownloadURL().then(function(url) {
 				deferred.resolve(url);
 			}).catch(function(error) {
   				deferred.reject('An error occured during download url request');
+			});
+
+			promise.success = function(fn) {
+				promise.then(fn);
+				return promise;
+			}
+			promise.error = function(fn) {
+				promise.then(null, fn);
+				return promise;
+			}
+
+			return promise;
+		},
+
+		uploadUserImage: function (uid, file) {
+			var uploadTask = imagesRef.child(uid + ".jpg").put(file);
+
+			uploadTask.on('state_changed', function(snapshot){
+
+			}, function(error) {
+				deferred.reject(error);
+			}, function() {
+				var downloadURL = uploadTask.snapshot.downloadURL;
+
+				deferred.resolve(downloadURL);
 			});
 
 			promise.success = function(fn) {
